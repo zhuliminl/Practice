1c4488a58dd28631229addf7c22bcf3f71ba524c
zhuliminl <zhuliminl@gamil.com>
2017-05-22 03:13:56 +0800

    删除不必要文件‘ ;

---
+++ MVC 模式学习/Modular/js/myModular.js
@@ -0,0 +1,55 @@
+
+var myModular = {
+	name : 'Saul',
+	age : 56, 
+	sayName : function() {
+		alert(this.name);
+	},
+	setName : function(newName) {
+		alert(newName);
+	}
+}
+
+myModular.sayName();
+myModular.setName('Quinn');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
