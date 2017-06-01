7e15d5df0146c431268c03e9f2e82752eae70d49
zhuliminl <zhuliminl@gmail.com>
2017-06-01 18:50:22 +0800

    get code pretty

--- jQuery & Plugins/Ajax/js/main.js
+++ jQuery & Plugins/Ajax/js/main.js
@@ -1,103 +1,86 @@
 $(function() {
 
-	var $orders =$('#orders');
-	var $name = $('#name');
-	var $drink = $('#drink');
+    var $orders = $('#orders');
+    var $name = $('#name');
+    var $drink = $('#drink');
 
-	// 模版生成
-	var orderTemplate = $('#order-template').html();
-		
-	function  addOrder(order) {
-		$orders.append(Mustache.render(orderTemplate, order));
-	} 
-	
- 	// GET 方法jkj
-	$.ajax({
-		type : 'GET',
-		url : 'http://rest.learncode.academy/api/learncode/friends',
-		// url : 'orders/API.json',
-		success : function(data) {
-			console.table(data) ;
+    // 模版生成
+    var orderTemplate = $('#order-template').html();
 
-			$.each(data, function(i, order) {
-				addOrder(order);
-			});
+    function addOrder(order) {
+        $orders.append(Mustache.render(orderTemplate, order));
+    }
 
-			// $orders.find("li").eq(35).prevAll("").remove();
+    // GET 方法jkj
+    $.ajax({
+        type: 'GET',
+        url: 'http://rest.learncode.academy/api/learncode/friends',
+        // url : 'orders/API.json',
+        success: function(data) {
+            console.table(data);
 
-		},
-		error : function() {
-			console.log('error');
-		}
+            $.each(data, function(i, order) {
+                addOrder(order);
+            });
 
-	});
+            // $orders.find("li").eq(35).prevAll("").remove();
 
-	// 
-	$('#add-order').on("click", function(e) {
-		e.preventDefault();
-		console.log('k');
+        },
+        error: function() {
+            console.log('error');
+        }
 
-		var order = {
-			name : $name.val(),
-			drink : $drink.val()
-		};
-		console.log(order);
+    });
 
-		$.ajax({
+    // 
+    $('#add-order').on("click", function(e) {
+        e.preventDefault();
+        console.log('k');
 
-			type : 'POST',
-			url : 'http://rest.learncode.academy/api/learncode/frie nds',
-			data : order, // 注意这里是我们需要提交的数据 方式是 POST
-			success : function(newOrder) {
-				addOrder(newOrder); 
-			},
-			error : function() {
-				console.log('Fail to Post')
-			}
+        var order = {
+            name: $name.val(),
+            drink: $drink.val()
+        };
+        console.log(order);
 
-		});
+        $.ajax({
 
-		
-	});
+            type: 'POST',
+            url: 'http://rest.learncode.academy/api/learncode/frie nds',
+            data: order, // 注意这里是我们需要提交的数据 方式是 POST
+            success: function(newOrder) {
+                addOrder(newOrder);
+            },
+            error: function() {
+                console.log('Fail to Post')
+            }
 
-	$orders.delegate(".remove","click", function() {
+        });
 
-			var $li = $(this).closest("li");
-			var self = this;
-			$.ajax({
-				type : 'DELETE',
-				url : 'http://rest.learncode.academy/api/learncode/friends/' + $(this).attr('data-id'),
-				success : function() {
-					console.log($(self));
-					$li.fadeOut(300, function() {
-						$(this).remove();
-						console.log($(this));
-					});
-				}
-			});
-			console.log('nihao');
-		});
 
-	$orders.delegate(".editOrder", "click", function() {
-		var $li = $(this).closest("li");
+    });
 
-		$li.find("input.name").val( $(li).find("span.name").html() );
-	} )
+    $orders.delegate(".remove", "click", function() {
+
+        var $li = $(this).closest("li");
+        var self = this;
+        $.ajax({
+            type: 'DELETE',
+            url: 'http://rest.learncode.academy/api/learncode/friends/' + $(this).attr('data-id'),
+            success: function() {
+                console.log($(self));
+                $li.fadeOut(300, function() {
+                    $(this).remove();
+                    console.log($(this));
+                });
+            }
+        });
+        console.log('nihao');
+    });
+
+    $orders.delegate(".editOrder", "click", function() {
+        var $li = $(this).closest("li");
+
+        $li.find("input.name").val($(li).find("span.name").html());
+    })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- jQuery & Plugins/照片查看器/js/main.js
+++ jQuery & Plugins/照片查看器/js/main.js
@@ -1,77 +1,74 @@
 var picViewer = $(function() {
-	// cache DOM
-	var $photoViewer = $('#photo-viewer');
-	var $thumbnails = $('#thumbnails');
+    // cache DOM
+    var $photoViewer = $('#photo-viewer');
+    var $thumbnails = $('#thumbnails');
 
-	// design data 
-	var request; // 路径验证变量
-	var cache = {}; // 缓存对象
-	var $current; // 当前图片
+    // design data 
+    var request; // 路径验证变量
+    var cache = {}; // 缓存对象
+    var $current; // 当前图片
 
-	// bind events
-	$thumbnails.on('click', 'a', switchPic);
-	$thumbnails.on('mouseover', 'a', switchPic);
-	$thumbnails.find('a').eq(0).click();
-	function switchPic(e) {
-		e.preventDefault();
-		$thumbnails.find('a').removeClass('active');
-		$(this).addClass('active');
+    // bind events
+    $thumbnails.on('click', 'a', switchPic);
+    $thumbnails.on('mouseover', 'a', switchPic);
+    $thumbnails.find('a').eq(0).click();
 
-		var src = this.href; // 取到路径
-		 	request = src; // 交给验证变量
-		var $img = $('<img/>');
+    function switchPic(e) {
+        e.preventDefault();
+        $thumbnails.find('a').removeClass('active');
+        $(this).addClass('active');
 
-		if(cache.hasOwnProperty(src)) { // 如果缓存里面有了当前路径
+        var src = this.href; // 取到路径
+        request = src; // 交给验证变量
+        var $img = $('<img/>');
 
-			if(cache[src].isLoading === false) { // 如果加载完毕
-				showPic(cache[src].$img); // 显示缓存中的图片
-			}
-		} else { // 设计缓存并加载一次图片
+        if (cache.hasOwnProperty(src)) { // 如果缓存里面有了当前路径
 
-			cache[src] = { // 设计缓存状态
-				$img: $img, // 缓存图片
-				isLoading: true // 记录状态，设为 true，在加载完成时取反值
-			};
+            if (cache[src].isLoading === false) { // 如果加载完毕
+                showPic(cache[src].$img); // 显示缓存中的图片
+            }
+        } else { // 设计缓存并加载一次图片
 
-			$photoViewer.addClass('is-loading');
-			$img.attr({
-				src: src,
-				title: this.title
-			})
-			// 图片加载进来 
-			$img.on('load', function() {
+            cache[src] = { // 设计缓存状态
+                $img: $img, // 缓存图片
+                isLoading: true // 记录状态，设为 true，在加载完成时取反值
+            };
 
-				$img.hide();  
-				$photoViewer.removeClass('is-loading').append($img); //添加上图片元素 
-				cache[src].isLoading = false; // 加载结束，修改加载状态
+            $photoViewer.addClass('is-loading');
+            $img.attr({
+                    src: src,
+                    title: this.title
+                })
+                // 图片加载进来 
+            $img.on('load', function() {
 
-				if(request === src) { // 验证图片路径
-					showPic($img); // 显示图片
-				}
-				// 点击事件会按照点击顺序进行，但是加载事件则按照加载时间的不同，会在不同的时间段完成，所以需要在加载图片时配对点击事件中的 src 和加载事件中的 src，才能确保加载的图片总是指向点击事件中的 src
+                $img.hide();
+                $photoViewer.removeClass('is-loading').append($img); //添加上图片元素 
+                cache[src].isLoading = false; // 加载结束，修改加载状态
 
-			});
+                if (request === src) { // 验证图片路径
+                    showPic($img); // 显示图片
+                }
+                // 点击事件会按照点击顺序进行，但是加载事件则按照加载时间的不同，会在不同的时间段完成，所以需要在加载图片时配对点击事件中的 src 和加载事件中的 src，才能确保加载的图片总是指向点击事件中的 src
 
-			
-		}
+            });
 
 
-	}
+        }
 
-	function showPic($img) {
 
-		if($current) {
-			$current.stop().hide();
-		}
-		$img.stop().fadeIn(1000);
+    }
 
-		$current = $img; // 更新
+    function showPic($img) {
 
-	}
+        if ($current) {
+            $current.stop().hide();
+        }
+        $img.stop().fadeIn(1000);
+
+        $current = $img; // 更新
+
+    }
 
 
 })
-
-
-
-
